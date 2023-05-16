9cb6a65bd9fb646afb8d3e3f4f0428d1e951b0a6
Justin Acosta <>
2023-05-15 21:20:00 -0400

    added aboutMe component

---
+++ src/Components/AboutMe/AboutMe.js
@@ -0,0 +1,11 @@
+import react from "react";
+import "./AboutMe.css";
+
+function AboutMe() {
+    return (
+        <div className="about-me-container">
+
+        </div>
+    );
+};
+export default AboutMe;
